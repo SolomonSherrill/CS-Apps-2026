30b0eb5990612bebda63ed8ffe70a4656f1a580e
SolomonSherrill <sherrs27@polyprep.org>
2026-02-27 17:11:11 -0500

    login/auth methods

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from user_auth import user_auth
+from pydantic import BaseModel
 app = FastAPI()
+
+class AuthRequest(BaseModel):
+    username: str
+    password: str
+
+auth = user_auth()
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.post("/register")
+def register(request: AuthRequest):
+    return auth.create_user(request.username, request.password)
+
+@app.post("/login")
+def login(request: AuthRequest):
+    return auth.authenticate_user(request.username, request.password)
